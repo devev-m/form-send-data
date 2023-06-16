729be911088af5b66c558a739e6272c3f5a0c6f7
devev-m <evgeny.m.1922@gmail.com>
2023-06-16 22:42:48 +0300

    added and applied fonts

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import App from './App.tsx';
 import { BrowserRouter } from 'react-router-dom';
 
 import './index.scss';
+import './assets/fonts/fonts.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <BrowserRouter>
